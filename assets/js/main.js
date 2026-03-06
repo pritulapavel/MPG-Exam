80916915dff677907e8af73cc1e9d7d4b538d0f2
Mariami <mariam.tch1996@gmail.com>
2026-03-06 21:45:40 +0400

    edited discover html, activate js story scrolling on desktop, edited styles and  files accordingly

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,9 @@ function getAverageColor(img) {
 // <<<<<<<<<<<<<<<<<<<<<< Discover Page Scripts: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
 // ფუნქცია რომ ანიმაციები  ივსებოდეს ტანმიმდევრულად,
 // მხოლოდ იმ  სექციაში, რომელიც ვიუპორტში ხვდება
-const scrollSections = document.querySelectorAll(".scrolls");
+// ფუნქცია რომ ანიმაციები  ივსებოდეს ტანმიმდევრულად,
+// მხოლოდ იმ  სექციაში, რომელიც ვიუპორტში ხვდება
+const scrollSections = document.querySelectorAll(".scrolls, .scrolls-desktop");
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -77,24 +79,18 @@ const observer = new IntersectionObserver(
             "animationend",
             () => {
               index++;
-
-              if (index < bars.length) {
-                animateBars();
-              }
+              if (index < bars.length) animateBars();
             },
             { once: true },
           );
         }
 
         animateBars();
-
         observer.unobserve(entry.target);
       }
     });
   },
-  {
-    threshold: 0.6,
-  },
+  { threshold: 0.6 },
 );
 
 scrollSections.forEach((section) => observer.observe(section));
@@ -102,7 +98,7 @@ scrollSections.forEach((section) => observer.observe(section));
 //  Hide  fixed menu while scrolling down and  show it  when customer will scroll up
 let lastScroll = 0;
 const nav = document.querySelector(".mobile-filters nav");
-const main = document.querySelector(".main-discover");
+const main = document.querySelector(".main");
 main.addEventListener("scroll", () => {
   const currentScroll = main.scrollTop;
 
@@ -114,3 +110,4 @@ main.addEventListener("scroll", () => {
 
   lastScroll = currentScroll;
 });
+// Desktop Scroll bar
